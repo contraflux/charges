c40dc3483f00f737ab621da898139c84cc9c7d31
contraflux <eg.rosenfeld@icloud.com>
2025-10-08 14:50:52 -0400

    Initial commit

---
+++ src/js/components/Objects.js
@@ -0,0 +1,72 @@
+/**
+ * Objects
+ * 
+ * Classes for organizing content needed across scripts
+ * 
+ * @author contraflux
+ * @date 10/8/2025
+ */
+
+class Container {
+    constructor(id) {
+        this.canvas = document.getElementById(id);
+        this.ctx = this.canvas.getContext('2d');
+    }
+}
+
+export class FieldContainer extends Container {
+    constructor(id) {
+        super(id);
+        this.coordScale = 25;
+        this.offsetX = 0;
+        this.offsetY = 0;
+        this.isDragging = false;
+        this.dragging = null;
+        this.mouseOffsetX = 0;
+        this.mouseOffsetY = 0;
+        this.zoomSpeed = 2e-3;
+        this.overlay = "none";
+        this.chargeList = [];
+        this.dt = 0.1;
+        this.elapsedTime = 0;
+        this.colorOffset = 0;
+    }
+
+    resetFields() {
+        this.coordScale = 25;
+        this.offsetX = 0;
+        this.offsetY = 0;
+        this.isDragging = false;
+        this.mouseOffsetX = 0;
+        this.mouseOffsetY = 0;
+        this.initialLocations = [];
+        this.overlay = "none";
+        this.elapsedTime = 0;
+    }
+
+    dragGrid(e) {
+        if (!this.isDragging) return null;
+
+        this.offsetX += e.movementX / this.coordScale;
+        this.offsetY -= e.movementY / this.coordScale;
+    }
+
+    zoomGrid(e) {
+        this.coordScale += e.deltaY * this.coordScale * this.zoomSpeed;
+
+        if (this.coordScale < 1e-5) {
+            this.coordScale = 1e-5;
+        }
+    }
+}
+
+export class Charge {
+    constructor(x, y, v_x, v_y, q) {
+        this.x = x;
+        this.y = y;
+        this.v_x = v_x;
+        this.v_y = v_y;
+        this.q = q;
+        this.isLocked = false;
+    }
+}
